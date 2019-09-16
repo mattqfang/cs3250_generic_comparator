ccf567a5c3f9be4abbdba13822505963cbb867d7
mattqfang <mattqfang@yahoo.com>
2019-09-16 16:08:44 -0600

    initial the project

---
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -0,0 +1,9 @@
+package uvu.cs.cs3250.assignments;
+
+public interface GenericComparator {
+	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
+		//Hint: use instanceof to check the type
+		//      "The type is not supported" Exception will be thrown
+		throw new Exception("this class is not implemented yet!");
+	}
+}
