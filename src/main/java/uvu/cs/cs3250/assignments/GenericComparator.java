6cb6c02c2fb1ec695c53534fcaa7a5c94e1e7caf
Jasen Fonnesbeck <jfsoccerman@hotmail.com>
2019-11-11 17:40:08 -0700

    all tests passed

--- src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -1,9 +1,28 @@
 package uvu.cs.cs3250.assignments;
 
+import java.time.LocalDate;
+
 public interface GenericComparator {
 	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
 		//Hint: use instanceof to check the type
 		//      "The type is not supported" Exception will be thrown
-		throw new Exception("this class is not implemented yet!");
+		String word1, word2;
+		
+				
+		if(obj1 instanceof String && obj2 instanceof String) {			
+			//cast into string types, get rid of punctuation then !.equals()
+			word1 = ((String)obj1).replaceAll("[.,!?\\-]", "");
+			word2 = ((String)obj2).replaceAll("[.,!?\\-]", "");
+			return !word1.equalsIgnoreCase(word2);
+		}
+		else if(obj1 instanceof Long && obj2 instanceof Long) {
+			return obj1 != obj2;
+		}
+		else if(obj1 instanceof LocalDate && obj2 instanceof LocalDate) {
+			return obj1 != obj2;
+		}
+		else {
+			throw new Exception("Variables are of non-acceptable instances");
+		}
 	}
 }
