413c02bffb0f6e62da68de52d3d643dae19b0e72
Kyle Gray <kyeegray@gmail.com>
2019-11-15 22:35:09 -0700

    Function implemented, all tests passed.

--- src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -1,9 +1,18 @@
 package uvu.cs.cs3250.assignments;
 
+import java.time.LocalDate;
+
 public interface GenericComparator {
 	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
-		//Hint: use instanceof to check the type
-		//      "The type is not supported" Exception will be thrown
+		if (obj1 instanceof String && obj2 instanceof String) {
+			String temp1 = ((String) obj1).replaceAll("[^A-Za-z]", "");
+			String temp2 = ((String) obj2).replaceAll("[^A-Za-z]", "");
+			return !temp1.equals(temp2);
+		} else if (obj1 instanceof Long && obj2 instanceof Long) {
+			return !((Long) obj1).equals((Long) obj2);
+		} else if (obj1 instanceof LocalDate && obj2 instanceof LocalDate) {
+			return ((LocalDate) obj1).compareTo((LocalDate) obj2) != 0;
+		}
 		throw new Exception("this class is not implemented yet!");
 	}
 }
