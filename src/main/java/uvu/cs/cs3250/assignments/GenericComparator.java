2bce050c9901dc387ce4c664ae85a1ed42628f3a
Ben Larsen <benjamindlarse@gmail.com>
2019-11-18 18:15:08 -0700

    Implementation

--- src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -1,9 +1,17 @@
 package uvu.cs.cs3250.assignments;
+import java.time.LocalDate;
 
 public interface GenericComparator {
 	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
-		//Hint: use instanceof to check the type
-		//      "The type is not supported" Exception will be thrown
-		throw new Exception("this class is not implemented yet!");
+		if (!(obj1 instanceof String || obj1 instanceof Long || obj1 instanceof LocalDate)) {
+			throw new Exception("Unsupported type");
+		}
+		if (obj1 instanceof String && obj2 instanceof String) {
+			String string1 = (String) obj1;
+			String string2 = (String) obj2;
+			obj1 = (T) string1.replaceAll("[^a-zA-Z0-9]", "");
+			obj2 = (T) string2.replaceAll("[^a-zA-Z0-9]", "");
+		}
+		return !obj1.equals(obj2);
 	}
 }
