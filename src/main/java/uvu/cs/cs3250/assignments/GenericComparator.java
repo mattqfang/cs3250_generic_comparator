a74f2e7c410242b531aecdd017494887e7be4f38
keatoneyre <10752265@my.uvu.edu>
2019-11-20 23:33:59 -0700

    Passing unit tests, non-FP solution

--- src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -1,9 +1,15 @@
 package uvu.cs.cs3250.assignments;
 
+import java.time.LocalDate;
+
 public interface GenericComparator {
-	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
-		//Hint: use instanceof to check the type
-		//      "The type is not supported" Exception will be thrown
-		throw new Exception("this class is not implemented yet!");
+	static <T> boolean isDifferent(T obj1, T obj2) throws Exception 
+	{
+		if (!(obj1 instanceof String && obj2 instanceof String))
+			if (!(obj1 instanceof Long && obj2 instanceof Long))
+				if (!(obj1 instanceof LocalDate && obj2 instanceof LocalDate))
+					throw new Exception("Type not supported");
+		
+		return obj1.toString().replaceAll("[^a-zA-Z0-9]", "").equals(obj2.toString().replaceAll("[^a-zA-Z0-9]", "")) ? false : true;
 	}
 }
