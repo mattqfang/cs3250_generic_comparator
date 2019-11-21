30e2182fa211eced871cd649494c22bc8a46307e
Jordan <frigis.something@gmail.com>
2019-11-20 17:24:03 -0700

    project now works with tests

--- src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -1,9 +1,28 @@
 package uvu.cs.cs3250.assignments;
 
+import java.time.LocalDate;
+
 public interface GenericComparator {
 	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
 		//Hint: use instanceof to check the type
 		//      "The type is not supported" Exception will be thrown
-		throw new Exception("this class is not implemented yet!");
+		System.out.println(obj1.getClass().getName());
+		if (obj1 instanceof String && obj2 instanceof String) {
+			return isDifferentString((String) obj1, (String) obj2);
+		}
+		if (obj1 instanceof Long && obj2 instanceof Long) {
+			return (Long) obj1 != (Long) obj2;
+		}
+		if (obj1 instanceof LocalDate && obj2 instanceof LocalDate) {
+			return ((LocalDate) obj1).equals((LocalDate) obj2) == false;
+		}
+
+		throw new Exception("Parameter objects are not supported!");
+	}
+	
+	static boolean isDifferentString(String obj1, String obj2) {
+		obj1 = obj1.replaceAll("[^a-zA-Z0-9]", "");
+		obj2 = obj2.replaceAll("[^a-zA-Z0-9]", "");
+		return obj1.equals(obj2) == false;
 	}
 }
