301285ed5426dac277cdf0225ce3ce5397c71c89
McKay Larsen <mckay.larsen@microfocus.com>
2019-11-25 12:35:21 -0700

    test commit

--- src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -2,8 +2,10 @@ package uvu.cs.cs3250.assignments;
 
 public interface GenericComparator {
 	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
+		if (obj1 instanceof)
+		if(obj1 instanceof)
 		//Hint: use instanceof to check the type
 		//      "The type is not supported" Exception will be thrown
-		throw new Exception("this class is not implemented yet!");
+		throw new Exception("Uh-oh, these types aren't supported!");
 	}
 }
