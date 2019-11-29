6999f356e1cd83b73497d99ef6fd53f01b6eafaf
Spencer_Thompson <themalewolf@gmail.com>
2019-11-29 12:36:11 -0700

    All working except Exception

--- src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -4,6 +4,37 @@ public interface GenericComparator {
 	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
 		//Hint: use instanceof to check the type
 		//      "The type is not supported" Exception will be thrown
-		throw new Exception("this class is not implemented yet!");
+		
+		//String
+		if (obj1 instanceof String){
+		String object1 = ((String) obj1).replaceAll("[^a-z]", "");
+		String object2 = ((String) obj2).replaceAll("[^a-z]", "");
+		
+		if(object1.equals(object2)) {
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+		//Long
+	if (obj1 instanceof Long){
+		if(obj1.equals(obj2)) {
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+		//Date
+	
+		if(obj1.equals(obj2)) {
+			return false;
+		}
+		else{
+			return true;
+		}
+	
+		//throw new Exception("this class is not implemented yet!");
 	}
 }
