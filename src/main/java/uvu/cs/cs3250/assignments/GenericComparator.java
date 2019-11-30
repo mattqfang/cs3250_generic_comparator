0a9a9520f4141ad0f0ad27175a690173b4b93e9e
Cj Davis <c.j._d@hotmail.com>
2019-11-30 16:57:31 -0700

    initial commit?

--- src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
+++ src/main/java/uvu/cs/cs3250/assignments/GenericComparator.java
@@ -1,9 +1,27 @@
 package uvu.cs.cs3250.assignments;
+//class Test<T>
+//{
+//	T obj;
+//	Test(T obj){this.obj = obj;}
+//	public T getObject() {return this.obj;}
+//}
+
 
 public interface GenericComparator {
 	static <T> boolean isDifferent(T obj1, T obj2) throws Exception {
+		T val1 = obj1;
+		T val2 = obj2;
+		if(val2 instanceof val1 == true) {
+			if(obj1==obj2) {
+				return false;
+			}
+			else return true;
+			
+		}
 		//Hint: use instanceof to check the type
 		//      "The type is not supported" Exception will be thrown
-		throw new Exception("this class is not implemented yet!");
+		else {
+		throw new Exception("The type is not supported");
+		}
 	}
 }
